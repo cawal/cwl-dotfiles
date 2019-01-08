f3be9b2f090c4deca7c8bcd4d4b944af541f971f
CaWaL <ricardo.cawal@gmail.com>
2019-01-08 01:28:48 -0200

    screen orientation for navi

---
+++ i3/home/cawal/.config/i3/screenlayout-navi.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+xrandr --output LVDS-1 --primary --mode 1366x768 --pos 768x0 --rotate normal --output DP-1 --off --output HDMI-1 --off --output VGA-1 --mode 1366x768 --pos 0x0 --rotate left
+compton --backend glx --vsync drm --glx-no-stencil --config ~/.config/i3/compton.conf
