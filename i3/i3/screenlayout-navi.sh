7b2c94887b2e4d674cc9e4c04a65b2a7ec27a2d1
cawal <ricardo.cawal@gmail.com>
2019-09-16 16:53:43 -0300

    screenlayout for navi

--- i3/i3/screenlayout-navi.sh
+++ i3/i3/screenlayout-navi.sh
@@ -1,3 +1,2 @@
 #!/bin/sh
-xrandr --output eDP-1 --primary --mode 1366x768 --pos 768x0 --rotate normal --output DP-1 --off --output HDMI-1 --mode 1366x768 --pos 0x0 --rotate left
-compton --backend glx --vsync drm --glx-no-stencil --config ~/.config/i3/compton.conf
+xrandr --output HDMI-1 --mode 1366x768 --pos 0x0 --rotate left --output eDP-1 --mode 1366x768 --pos 768x0 --rotate normal
