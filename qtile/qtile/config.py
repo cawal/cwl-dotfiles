d45fb31315c474b889679b672ef705bed3d5f236
cawal <ricardo.cawal@gmail.com>
2021-06-06 12:08:32 -0300

    Qtile: Correct k and j bindings

--- qtile/qtile/config.py
+++ qtile/qtile/config.py
@@ -59,21 +59,21 @@ def remove_group(name):
 keys = [
         # Switch between windows in current stack pane
         Key(
-            [mod], "k",
+            [mod], "j",
             lazy.layout.down()
             ),
         Key(
-            [mod], "j",
+            [mod], "k",
             lazy.layout.up()
             ),
 
         # Move windows up or down in current stack
         Key(
-            [mod, "control"], "k",
+            [mod, "control"], "j",
             lazy.layout.shuffle_down()
             ),
         Key(
-            [mod, "control"], "j",
+            [mod, "control"], "k",
             lazy.layout.shuffle_up()
             ),
 
