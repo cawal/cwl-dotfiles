5398f9d061cc9da2a73975a916bcd1d62e6238c4
cawal <ricardo.cawal@gmail.com>
2021-07-03 11:48:08 -0300

    Sync

---
+++ qtile/qtile/cwllayouts.py
@@ -0,0 +1,10 @@
+from libqtile.layout import TreeTab
+
+class CWLTreeTab(TreeTab):
+
+    def draw_panel(self, *args):
+        if not self._panel:
+            return
+        self._drawer.clear(self.bg_color)
+        self._tree.draw(self, 0)
+        self._drawer.draw(offsetx=0, width=self.panel_width)
