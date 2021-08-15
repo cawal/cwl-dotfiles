9b66abc7d83be4021935735b7344d1a4523b78ec
cawal <ricardo.cawal@gmail.com>
2021-08-15 13:09:24 -0300

    Qtile: add commented swallow function

--- qtile/qtile/config.py
+++ qtile/qtile/config.py
@@ -21,6 +21,27 @@ def autostart():
     home = os.path.expanduser("~/bin/cwl-startup-processes.sh")
     subprocess.call([home])
 
+#import psutil
+#@hook.subscribe.client_new
+#def _swallow(window):
+#    """Imported from: https://github.com/GroosL/dotfiles/blob/main/config/qtile/config.py"""
+#    pid = window.window.get_net_wm_pid()
+#    ppid = psutil.Process(pid).ppid()
+#    cpids = {c.window.get_net_wm_pid(): wid for wid, c in window.qtile.windows_map.items()}
+#    for i in range(5):
+#        if not ppid:
+#            return
+#        if ppid in cpids:
+#            parent = window.qtile.windows_map.get(cpids[ppid])
+#            parent.minimized = True
+#            window.parent = parent
+#            return
+#        ppid = psutil.Process(ppid).ppid()
+#
+#@hook.subscribe.client_killed
+#def _unswallow(window):
+#    if hasattr(window, 'parent'):
+#        window.parent.minimized = False
 
 mod = "mod4"
 
