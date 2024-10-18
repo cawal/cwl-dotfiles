f80ddee0cd0206beed1db4880722dbf5e4c30014
cawal <ricardo.cawal@gmail.com>
2024-10-18 18:10:03 -0300

    neovim: obsidian.nvim base

--- neovim/config/init.lua
+++ neovim/config/init.lua
@@ -903,6 +903,40 @@ require("lazy").setup({
 		---@type render.md.UserConfig
 		opts = {},
 	},
+	{
+		"epwalsh/obsidian.nvim",
+		version = "*", -- recommended, use latest release instead of latest commit
+		lazy = true,
+		ft = "markdown",
+		-- Replace the above line with this if you only want to load obsidian.nvim for markdown files in your vault:
+		-- event = {
+		--   -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
+		--   -- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/*.md"
+		--   -- refer to `:h file-pattern` for more examples
+		--   "BufReadPre path/to/my-vault/*.md",
+		--   "BufNewFile path/to/my-vault/*.md",
+		-- },
+		dependencies = {
+			-- Required.
+			"nvim-lua/plenary.nvim",
+
+			-- see below for full list of optional dependencies 👇
+		},
+		opts = {
+			workspaces = {
+				{
+					name = "personal",
+					path = "~/Zettelkasten",
+				},
+				{
+					name = "work",
+					path = "~/Liber-PKM",
+				},
+			},
+			-- see below for full list of options 👇
+			ui = { enable = false },
+		},
+	},
 
 	-- The following two comments only work if you have downloaded the kickstart repo, not just copy pasted the
 	-- init.lua. If you want these files, they are in the repository, so you can just download them and
