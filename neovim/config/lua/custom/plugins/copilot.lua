5f1c5de477b27a5b4523f4e1eebf4ab0c789acb3
cawal <ricardo.cawal@gmail.com>
2026-02-25 16:33:36 -0300

    feat(neovim): add Copilot enable/disable keys and filetypes
    Added keymaps <leader>c0 and <leader>c1 to quickly disable or enable Copilot
    in normal mode. Configured Copilot to activate for all filetypes and
    explicitly for markdown. Improved accessibility and control of Copilot
    integration in Neovim.

--- neovim/config/lua/custom/plugins/copilot.lua
+++ neovim/config/lua/custom/plugins/copilot.lua
@@ -5,6 +5,12 @@ return {
 			expr = true,
 			replace_keycodes = false,
 		})
+		vim.keymap.set("n", "<leader>c0", ":Copilot disable<CR>", { desc = "Disable Copilot" })
+		vim.keymap.set("n", "<leader>c1", ":Copilot enable<CR>", { desc = "Enable Copilot" })
 		vim.g.copilot_no_tab_map = true
+		vim.g.copilot_filetypes = {
+			["*"] = true,
+			["markdown"] = true,
+		}
 	end,
 }
