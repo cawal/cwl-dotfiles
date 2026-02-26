748a2d7c2836e40e4198a7eef4a2961684c3eb7b
cawal <ricardo.cawal@gmail.com>
2026-02-26 11:14:17 -0300

    feat(neovim): update obsidian config with note and frontmatter
    - Added explicit `note.template = vim.NIL` to disable note templates.
    - Disabled frontmatter metadata merging by setting `frontmatter.enabled = false`.
    - Referenced default options for clarity and maintainability.

--- neovim/config/lua/custom/plugins/obsidian.lua
+++ neovim/config/lua/custom/plugins/obsidian.lua
@@ -4,13 +4,19 @@ return {
 	ft = "markdown",
 	---@module 'obsidian'
 	---@type obsidian.config
-	opts = {
+	opts = { -- default options: https://github.com/obsidian-nvim/obsidian.nvim/blob/main/lua/obsidian/config/default.lua
 		legacy_commands = false, -- this will be removed in the next major release
 		workspaces = {
 			{
 				name = "personal",
 				path = "~/Zettelkasten",
 			},
+		},
+		note = {
+			template = vim.NIL,
+		},
+		frontmatter = {
+			enabled = false, -- enable frontmatter to add metadata to your notes
 		},
 	},
 }
